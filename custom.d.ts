dbb1664b63b550d8a1636d34280824b754c8b083
Аксенов Павел Игоревич <aksenovpi@sibintek.ru>
2021-07-05 17:50:10 +0300

    added custom.d.ts

---
+++ custom.d.ts
@@ -0,0 +1,14 @@
+declare namespace Express {
+    export interface Request {
+        ClientInfo: {
+            readonly connectDate: Date
+            requestUrl: string
+            requestMethod: string
+            requestCookies: string
+            requestSignedCookies: string
+            requestIP: string
+            body: object
+            queryParams: object
+        }
+    }
+}

0f853f2b573a0cc1b09778c007f22ae74a0daade
Аксенов Павел Игоревич <aksenovpi@sibintek.ru>
2021-07-07 15:15:38 +0300

    fix

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,12 +1,20 @@
 import { Express, NextFunction, Request, Response } from 'express'
-import { EventName } from './emitters/emitters'
 
 export interface BlackBoxApp extends Express {}
 
 /**
  * Перечисление событий сервера
  */
-export const BlackBoxEventName = EventName
+export const enum BlackBoxEventName {
+    CLIENT_REQUEST = 'CLIENT_REQUEST',
+    SERVER_IS_RUNNING = 'SERVER_IS_RUNNING',
+    SERVER_IS_STOPPED = 'SERVER_IS_STOPPED',
+    DB_IS_CONNECTED = 'DB_IS_CONNECTED',
+    AMQP_IS_RECONNECTING = 'AMQP_IS_RECONNECTING',
+    AMQP_IS_CONNECTED = 'AMQP_IS_CONNECTED',
+    AMQP_CHANNEL_IS_CLOSED = 'AMQP_CHANNEL_IS_CLOSED',
+    AMQP_WORKER_IS_STARTED = 'AMQP_WORKER_IS_STARTED',
+}
 
 // == Сущности
 

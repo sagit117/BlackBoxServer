f30e09dac2a51672542e0ba01de0a717d32ed9a7
Аксенов Павел Игоревич <aksenovpi@sibintek.ru>
2021-07-07 15:45:11 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -117,119 +117,6 @@ export function createApp(env: NodeJS.ProcessEnv) {
     // errors
     BlackBoxApp.use(onErrorRequest)
 
-    /** Служебные функции */
-
-    /**
-     * Обработка ошибок в ходе запроса по http
-     * @param error     - ошибка
-     * @param _request
-     * @param response  - response
-     * @param _next
-     */
-    function onErrorRequest(
-        error: Error,
-        _request: Request,
-        response: Response,
-        _next: NextFunction
-    ) {
-        BlackBoxApp.emit('errorLog', error, 'REQUEST')
-
-        if (error instanceof URIError) {
-            return response.status(StatusCode.BAD_REQUEST).send('oops ...')
-        }
-
-        if (error instanceof HttpUnauthorizedException) {
-            return response.status(StatusCode.UNAUTHORIZED).send(error.message)
-        }
-
-        return response
-            .status(StatusCode.INTERNAL_SERVER_ERROR)
-            .send(error.message)
-    }
-
-    /**
-     * Собираем данные подключения http
-     * @param request   - данные запроса
-     * @param _response
-     * @param next
-     */
-    function onRequest(
-        request: clientRequest,
-        _response: Response,
-        next: NextFunction
-    ) {
-        const clientInfo = new ClientInfo(request)
-
-        request.ClientInfo = clientInfo.toObject()
-
-        BlackBoxApp.emit(
-            'eventLog',
-            EventName.CLIENT_REQUEST,
-            clientInfo.toString()
-        )
-
-        next()
-    }
-
-    /**
-     * Проверка работоспособности приложения
-     * @param _request
-     * @param response
-     * @param _next
-     */
-    function ping(_request: Request, response: Response, _next: NextFunction) {
-        return response.status(StatusCode.OK).send('OK')
-    }
-
-    /**
-     * Проверка состояния приложения
-     * @param _request
-     * @param response
-     * @param _next
-     */
-    function healthCheck(
-        _request: Request,
-        response: Response,
-        _next: NextFunction
-    ) {
-        /**
-         * Если подключение к rabbit или к БД не доступно
-         * вернем 404 код
-         */
-        if (
-            (getConfig().USE_RABBIT && !StatusAppConnect.connectedRabbit) ||
-            !StatusAppConnect.connectedDB
-        ) {
-            return response
-                .status(StatusCode.NOT_FOUND)
-                .send(JSON.stringify(StatusAppConnect))
-        }
-
-        return response.status(StatusCode.OK).send('OK')
-    }
-
-    /**
-     * Установка заголовков
-     * @param _request
-     * @param response
-     * @param next
-     */
-    function setHeader(
-        _request: Request,
-        response: Response,
-        next: NextFunction
-    ) {
-        const headers: THeader[] = getConfig().HEADERS
-
-        if (headers && Array.isArray(headers)) {
-            headers.forEach((head) => {
-                response.setHeader(head?.key, head?.value)
-            })
-        }
-
-        next()
-    }
-
     /**
      * Процесс создания сервера и подключения к сервисам
      */
@@ -285,4 +172,111 @@ export function BlackBoxRouter() {
  */
 export function BlackBoxBaseController() {
     return BaseController
+}
+
+/** Служебные функции */
+
+/**
+ * Обработка ошибок в ходе запроса по http
+ * @param error     - ошибка
+ * @param _request
+ * @param response  - response
+ * @param _next
+ */
+function onErrorRequest(
+    error: Error,
+    _request: Request,
+    response: Response,
+    _next: NextFunction
+) {
+    BlackBoxApp.emit('errorLog', error, 'REQUEST')
+
+    if (error instanceof URIError) {
+        return response.status(StatusCode.BAD_REQUEST).send('oops ...')
+    }
+
+    if (error instanceof HttpUnauthorizedException) {
+        return response.status(StatusCode.UNAUTHORIZED).send(error.message)
+    }
+
+    return response.status(StatusCode.INTERNAL_SERVER_ERROR).send(error.message)
+}
+
+/**
+ * Собираем данные подключения http
+ * @param request   - данные запроса
+ * @param _response
+ * @param next
+ */
+function onRequest(
+    request: clientRequest,
+    _response: Response,
+    next: NextFunction
+) {
+    const clientInfo = new ClientInfo(request)
+
+    request.ClientInfo = clientInfo.toObject()
+
+    BlackBoxApp.emit(
+        'eventLog',
+        EventName.CLIENT_REQUEST,
+        clientInfo.toString()
+    )
+
+    next()
+}
+
+/**
+ * Проверка работоспособности приложения
+ * @param _request
+ * @param response
+ * @param _next
+ */
+function ping(_request: Request, response: Response, _next: NextFunction) {
+    return response.status(StatusCode.OK).send('OK')
+}
+
+/**
+ * Проверка состояния приложения
+ * @param _request
+ * @param response
+ * @param _next
+ */
+function healthCheck(
+    _request: Request,
+    response: Response,
+    _next: NextFunction
+) {
+    /**
+     * Если подключение к rabbit или к БД не доступно
+     * вернем 404 код
+     */
+    if (
+        (getConfig().USE_RABBIT && !StatusAppConnect.connectedRabbit) ||
+        !StatusAppConnect.connectedDB
+    ) {
+        return response
+            .status(StatusCode.NOT_FOUND)
+            .send(JSON.stringify(StatusAppConnect))
+    }
+
+    return response.status(StatusCode.OK).send('OK')
+}
+
+/**
+ * Установка заголовков
+ * @param _request
+ * @param response
+ * @param next
+ */
+function setHeader(_request: Request, response: Response, next: NextFunction) {
+    const headers: THeader[] = getConfig().HEADERS
+
+    if (headers && Array.isArray(headers)) {
+        headers.forEach((head) => {
+            response.setHeader(head?.key, head?.value)
+        })
+    }
+
+    next()
 }
